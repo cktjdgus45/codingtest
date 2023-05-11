7d02ed68d0edae31ba34df27984f81346627f8cd
pooh <cktjdgus45@naver.com>
2023-05-11 11:11:17 +0900

    Eliminate duplicates 2

--- app.cpp
+++ app.cpp
@@ -1,28 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-map<int, int> mp;
-
+vector<int> v{1, 1, 2, 2, 3, 3, 5, 6, 7, 8, 9};
 int main()
 {
-    vector<int> v{1, 1, 2, 2, 3, 3};
+    sort(v.begin(), v.end());
+    auto it = v.erase(unique(v.begin(), v.end()), v.end());
     for (int i : v)
-    {
-        if (mp[i])
-        {
-            continue;
-        }
-        else
-        {
-            mp[i] = 1;
-        }
-    }
-    vector<int> ret;
-    for (auto it : mp)
-    {
-        ret.push_back(it.first);
-    }
-    for (int i : ret)
-        cout
-            << i << "\n";
+        cout << i << " ";
+    cout << '\n';
+    return 0;
 }
