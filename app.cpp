f4bc0dca8bef6fee1821c36e727375e09dcaa499
pooh <cktjdgus45@naver.com>
2023-05-17 21:12:34 +0900

    누적합-psum

--- app.cpp
+++ app.cpp
@@ -1,12 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> v{1, 1, 2, 2, 3, 3, 5, 6, 7, 8, 9};
+typedef long long ll;
+int a[100004], b, c, psum[100004], n, m;
 int main()
 {
-    sort(v.begin(), v.end());
-    auto it = v.erase(unique(v.begin(), v.end()), v.end());
-    for (int i : v)
-        cout << i << " ";
-    cout << '\n';
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        psum[i] = psum[i - 1] + a[i];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> b >> c;
+        cout << psum[c] - psum[b - 1] << "\n";
+    }
     return 0;
 }
