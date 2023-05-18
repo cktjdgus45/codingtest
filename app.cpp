ac15117afff73482635cfe2eb6c54a6d970d4305
pooh <cktjdgus45@naver.com>
2023-05-18 15:09:01 +0900

    팰린드롬

--- app.cpp
+++ app.cpp
@@ -1,27 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-int A, B, C, a, b, cnt[104], ret;
+string s, temp;
 int main()
 {
-    cin >> A >> B >> C;
-    for (int i = 0; i < 3; i++)
-    {
-        cin >> a >> b;
-        for (int j = a; j < b; j++)
-            cnt[j]++;
-    }
-    for (int j = 1; j < 100; j++)
-    {
-        if (cnt[j])
-        {
-            if (cnt[j] == 1)
-                ret += A;
-            else if (cnt[j] == 2)
-                ret += B * 2;
-            else if (cnt[j] == 3)
-                ret += C * 3;
-        }
-    }
-    cout << ret << "\n";
+    cin >> s;
+    temp = s;
+    reverse(temp.begin(), temp.end());
+    if (temp == s)
+        cout << 1 << "\n";
+    else
+        cout << 0 << "\n";
+
     return 0;
 }
