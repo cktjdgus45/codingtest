5f40c8a138fe9a06559b31190b9744d2c2caaed4
pooh <cktjdgus45@naver.com>
2023-05-19 19:16:48 +0900

    9375:패션왕 신해빈

--- app.cpp
+++ app.cpp
@@ -1,27 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int n, m;
-string s; 
-map<string, int> mp;
-map<int, string> mp2;
-string a[100004];
-int main(){  
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	cin >> n >> m; 
-	for(int i = 0; i < n; i++){
-		cin >> s; 
-		mp[s] = i + 1;  
-		mp2[i + 1] = s; 
-        a[i + 1] = s;
-	}
-	for(int i = 0; i < m; i++){
-		cin >> s; 
-		if(atoi(s.c_str()) == 0){
-			cout << mp[s] << "\n";
-		}else{
-			cout << a[atoi(s.c_str())] << "\n"; 
-			//cout << mp2[atoi(s.c_str())] << "\n"; 
-		}
-	} 
+int t, n;
+string a, b;
+int main(){
+    cin >> t;
+    while(t--){
+        map<string, int> _map;
+        cin >> n;
+         for(int i = 0; i < n; i++){
+            cin >> a >> b;
+            _map[b]++;
+        }
+        long long ret = 1;
+        for(auto c : _map){
+            ret *= ((long long)c.second + 1);
+        }
+        ret--;
+        cout << ret << "\n";
+    }
+    return 0;
 }
