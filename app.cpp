624790be364694fb1fc4411396172202c88098d8
pooh <cktjdgus45@naver.com>
2023-05-19 16:12:58 +0900

    1620:나는야 포켓몬 마스터 이다솜

--- app.cpp
+++ app.cpp
@@ -1,22 +1,27 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-int n, k, temp, psum[100001], ret = -1000000;
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> n >> k;
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> temp;
-        psum[i] = psum[i - 1] + temp;
-    }
-    for (int i = k; i <= n; i++)
-    {
-        ret = max(ret, psum[i] - psum[i - k]);
-    }
-    cout << ret << "\n";
-    return 0;
+int n, m;
+string s; 
+map<string, int> mp;
+map<int, string> mp2;
+string a[100004];
+int main(){  
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n >> m; 
+	for(int i = 0; i < n; i++){
+		cin >> s; 
+		mp[s] = i + 1;  
+		mp2[i + 1] = s; 
+        a[i + 1] = s;
+	}
+	for(int i = 0; i < m; i++){
+		cin >> s; 
+		if(atoi(s.c_str()) == 0){
+			cout << mp[s] << "\n";
+		}else{
+			cout << a[atoi(s.c_str())] << "\n"; 
+			//cout << mp2[atoi(s.c_str())] << "\n"; 
+		}
+	} 
 }
