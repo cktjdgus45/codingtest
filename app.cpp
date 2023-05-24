0911f7aaeef9326e0d74bc56c6903d38b9f08605
pooh <cktjdgus45@naver.com>
2023-05-24 14:14:21 +0900

    1012:유기농배추

--- app.cpp
+++ app.cpp
@@ -1,38 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int max_n = 104;
 int dy[4] = {-1, 0, 1, 0};
 int dx[4] = {0, 1, 0, -1};
-int n, m, a[max_n][max_n], visited[max_n][max_n], y, x;
+int m, n, k, y, x, ret, ny, nx, t;
+int a[51][51];
+bool visited[51][51];
+void dfs(int y, int x)
+{
+	visited[y][x] = 1;
+	for (int i = 0; i < 4; i++)
+	{
+		ny = y + dy[i];
+		nx = x + dx[i];
+		if (ny < 0 || nx < 0 || ny >= n || nx >= m)
+			continue;
+		if (a[ny][nx] == 1 && !visited[ny][nx])
+		{
+			dfs(ny, nx);
+		}
+	}
+	return;
+}
+
 int main()
 {
-	scanf("%d %d", &n, &m);
-	for (int i = 0; i < n; i++)
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> t;
+	while (t--)
 	{
-		for (int j = 0; j < m; j++)
+		fill(&a[0][0], &a[0][0] + 51 * 51, 0);
+		fill(&visited[0][0], &visited[0][0] + 51 * 51, 0);
+		ret = 0;
+		cin >> m >> n >> k;
+		for (int i = 0; i < k; i++)
 		{
-			scanf("%1d", &a[i][j]);
+			cin >> x >> y;
+			a[y][x] = 1;
 		}
-	}
-	queue<pair<int, int>> q;
-	visited[0][0] = 1;
-	q.push({0, 0});
-	while (q.size())
-	{
-		tie(y, x) = q.front();
-		q.pop();
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < n; i++)
 		{
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if (ny < 0 || ny >= n || nx < 0 || nx >= m || a[ny][nx] == 0)
-				continue;
-			if (visited[ny][nx])
-				continue;
-			visited[ny][nx] = visited[y][x] + 1;
-			q.push({ny, nx});
+			for (int j = 0; j < m; j++)
+			{
+				if (a[i][j] == 1 && !visited[i][j])
+				{
+					dfs(i, j);
+					ret++;
+				}
+			}
 		}
+		cout << ret << "\n";
 	}
-	printf("%d", visited[n - 1][m - 1]);
 	return 0;
 }
