b4fddc6a90708ad77b6bd8207ca72b298e35c644
pooh <cktjdgus45@naver.com>
2023-05-25 09:24:09 +0900

    1992 : 쿼드트리

--- app.cpp
+++ app.cpp
@@ -1,51 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[101][101], visited[101][101], n, ret = 1;
-int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
-void dfs(int y, int x, int d)
+typedef long long int ll;
+int n;
+string s;
+char a[101][101];
+string quard(int y, int x, int size)
 {
-	visited[y][x] = 1;
-	for (int i = 0; i < 4; i++)
-	{
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if (ny < 0 || nx < 0 || ny >= n || nx >= n)
-			continue;
-		if (!visited[ny][nx] && a[ny][nx] > d)
-			dfs(ny, nx, d);
-	}
-	return;
+    if (size == 1)
+        return string(1, a[y][x]);
+    char b = a[y][x];
+    string ret = "";
+    for (int i = y; i < y + size; i++)
+    {
+        for (int j = x; j < x + size; j++)
+        {
+            if (b != a[i][j])
+            {
+                ret += '(';
+                ret += quard(y, x, size / 2);
+                ret += quard(y, x + size / 2, size / 2);
+                ret += quard(y + size / 2, x, size / 2);
+                ret += quard(y + size / 2, x + size / 2, size / 2);
+                ret += ')';
+                return ret;
+            }
+        }
+    }
+    return string(1, a[y][x]);
 }
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			cin >> a[i][j];
-		}
-	}
-	for (int d = 1; d < 101; d++)
-	{
-		fill(&visited[0][0], &visited[0][0] + 101 * 101, 0);
-		int cnt = 0;
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				if (a[i][j] > d && !visited[i][j])
-				{
-					dfs(i, j, d);
-					cnt++;
-				}
-			}
-		}
-		ret = max(ret, cnt);
-	}
-	cout << ret << '\n';
-	return 0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s;
+        for (int j = 0; j < n; j++)
+        {
+            a[i][j] = s[j];
+        }
+    }
+    cout << quard(0, 0, n) << '\n';
+    return 0;
 }
