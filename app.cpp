5bd401f1e9460f4d86526eb4c795983b90f1d39b
pooh <cktjdgus45@naver.com>
2023-05-25 11:13:17 +0900

    2828 - 사과게임

--- app.cpp
+++ app.cpp
@@ -1,46 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
-int n;
-string s;
-char a[101][101];
-string quard(int y, int x, int size)
+int n, m, j, l, r, temp, ret;
+int main()
 {
-    if (size == 1)
-        return string(1, a[y][x]);
-    char b = a[y][x];
-    string ret = "";
-    for (int i = y; i < y + size; i++)
+    cin >> n >> m >> j;
+    l = 1;
+    for (int i = 0; i < j; i++)
     {
-        for (int j = x; j < x + size; j++)
+        r = l + m - 1;
+        cin >> temp;
+        if (temp >= l && temp <= r)
+            continue;
+        else
         {
-            if (b != a[i][j])
+            if (temp < l)
             {
-                ret += '(';
-                ret += quard(y, x, size / 2);
-                ret += quard(y, x + size / 2, size / 2);
-                ret += quard(y + size / 2, x, size / 2);
-                ret += quard(y + size / 2, x + size / 2, size / 2);
-                ret += ')';
-                return ret;
+                ret += (l - temp);
+                l = temp;
+            }
+            else
+            {
+                l += (temp - r);
+                ret += (temp - r);
             }
         }
     }
-    return string(1, a[y][x]);
-}
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> s;
-        for (int j = 0; j < n; j++)
-        {
-            a[i][j] = s[j];
-        }
-    }
-    cout << quard(0, 0, n) << '\n';
+    cout << ret << "\n";
     return 0;
 }
