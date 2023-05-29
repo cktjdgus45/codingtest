747babdf0513f7a89df5068dfb25562c80680266
pooh <cktjdgus45@naver.com>
2023-05-29 21:04:04 +0900

    2589 : 보물섬

--- app.cpp
+++ app.cpp
@@ -1,41 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, a[104][104];
-string s;
+int n, m, mx, visited[54][54];
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, 1, 0, -1};
+char a[54][54];
+void bfs(int y, int x)
+{
+    memset(visited, 0, sizeof(visited));
+    visited[y][x] = 1;
+    queue<pair<int, int>> q;
+    q.push({y, x});
+    while (q.size())
+    {
+        tie(y, x) = q.front();
+        q.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if (ny < 0 || ny >= n || nx < 0 || nx >= m)
+                continue;
+            if (visited[ny][nx])
+                continue;
+            if (a[ny][nx] == 'W')
+                continue;
+            visited[ny][nx] = visited[y][x] + 1;
+            q.push({ny, nx});
+            mx = max(mx, visited[ny][nx]);
+        }
+    }
+    return;
+}
 int main()
 {
-	cin >> n >> m;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> s;
-		for (int j = 0; j < m; j++)
-		{
-			if (s[j] == '.')
-				a[i][j] = -1;
-			else
-				a[i][j] = 0;
-		}
-	}
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			if (a[i][j] == 0)
-			{
-				int cnt = 1;
-				while (a[i][j + 1] == -1)
-				{
-					a[i][j + 1] = cnt++;
-					j++;
-				}
-			}
-		}
-	}
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-			cout << a[i][j] << " ";
-		cout << "\n";
-	}
-	return 0;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (a[i][j] == 'L')
+                bfs(i, j);
+        }
+    }
+    cout << mx - 1 << "\n";
 }
