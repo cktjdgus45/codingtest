e7bf2075682227e8e0e38699584f6a3f44de6a4b
pooh <cktjdgus45@naver.com>
2023-05-30 10:46:36 +0900

    16234 : 인구이동

--- app.cpp
+++ app.cpp
@@ -1,53 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, mx, visited[54][54];
+int visited[54][54], a[54][54], n, l, r, sum, cnt;
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
-char a[54][54];
-void bfs(int y, int x)
+vector<pair<int, int>> v;
+void dfs(int y, int x, vector<pair<int, int>> &v)
 {
-    memset(visited, 0, sizeof(visited));
-    visited[y][x] = 1;
-    queue<pair<int, int>> q;
-    q.push({y, x});
-    while (q.size())
+    for (int i = 0; i < 4; i++)
     {
-        tie(y, x) = q.front();
-        q.pop();
-        for (int i = 0; i < 4; i++)
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (nx < 0 || nx >= n || ny < 0 || ny >= n || visited[ny][nx])
+            continue;
+        if (abs(a[ny][nx] - a[y][x]) >= l && abs(a[ny][nx] - a[y][x]) <= r)
         {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (ny < 0 || ny >= n || nx < 0 || nx >= m)
-                continue;
-            if (visited[ny][nx])
-                continue;
-            if (a[ny][nx] == 'W')
-                continue;
-            visited[ny][nx] = visited[y][x] + 1;
-            q.push({ny, nx});
-            mx = max(mx, visited[ny][nx]);
+            visited[ny][nx] = 1;
+            v.push_back({ny, nx});
+            sum += a[ny][nx];
+            dfs(ny, nx, v);
         }
     }
-    return;
 }
+
 int main()
 {
-    cin >> n >> m;
+    cin >> n >> l >> r;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < n; j++)
         {
             cin >> a[i][j];
         }
     }
-    for (int i = 0; i < n; i++)
+    while (true)
     {
-        for (int j = 0; j < m; j++)
+        bool flag = 0;
+        fill(&visited[0][0], &visited[0][0] + 54 * 54, 0);
+        for (int i = 0; i < n; i++)
         {
-            if (a[i][j] == 'L')
-                bfs(i, j);
+            for (int j = 0; j < n; j++)
+            {
+                if (!visited[i][j])
+                {
+                    v.clear();
+                    visited[i][j] = 1;
+                    v.push_back({i, j});
+                    sum = a[i][j];
+                    dfs(i, j, v);
+                    if (v.size() == 1)
+                        continue;
+                    for (pair<int, int> b : v)
+                    {
+                        a[b.first][b.second] = sum / v.size();
+                        flag = 1;
+                    }
+                }
+            }
         }
+        if (!flag)
+            break;
+        cnt++;
     }
-    cout << mx - 1 << "\n";
+    cout << cnt << "\n";
+    return 0;
 }
