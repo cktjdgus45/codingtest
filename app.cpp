b24ecba8f844cb34645637de0934a3835c0189a7
pooh <cktjdgus45@naver.com>
2023-05-31 18:43:17 +0900

    13913 숨바꼭질4

--- app.cpp
+++ app.cpp
@@ -1,43 +1,58 @@
 #include <bits/stdc++.h>
-using namespace std;  
-
-vector<int> num; 
-vector<char> oper_str; 
-int n, ret = -987654321;
-string s; 
-void fastIO(){
+using namespace std;
+const int max_n = 500000;
+int visited[2][max_n + 4], a, b, ok, turn = 1;
+int main()
+{
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL); 
-    cout.tie(NULL);   
-} 
-int oper(char a, int b, int c){
-    if(a == '+') return b + c; 
-    if(a == '-') return b - c; 
-    if(a == '*') return b * c;  
-} 
-
-void go(int here, int _num){
-    if(here == num.size() - 1){ 
-        ret = max(ret, _num); 
-        return;
-    }  
-    go(here + 1, oper(oper_str[here], _num, num[here + 1]));
-
-    if(here + 2 <= num.size() - 1){
-        int temp = oper(oper_str[here + 1], num[here + 1], num[here + 2]); 
-        go(here + 2, oper(oper_str[here], _num, temp));  
-    } 
-    return;
-} 
-int main(){
-    fastIO();
-    cin >> n;  
-    cin >> s; 
-    for(int i = 0; i < n; i++){
-        if(i % 2 == 0)num.push_back(s[i] - '0');
-        else oper_str.push_back(s[i]);
-    } 
-    go(0, num[0]);  
-    cout << ret << "\n";
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> a >> b;
+    if (a == b)
+    {
+        cout << 0 << "\n";
+        return 0;
+    }
+    queue<int> q;
+    visited[0][a] = 1;
+    q.push(a);
+    while (q.size())
+    {
+        b += turn;
+        if (b > max_n)
+            break;
+        if (visited[turn % 2][b])
+        {
+            ok = true;
+            break;
+        }
+        int qSize = q.size();
+        for (int i = 0; i < qSize; i++)
+        {
+            int x = q.front();
+            q.pop();
+            for (int nx : {x + 1, x - 1, x * 2})
+            {
+                if (nx < 0 || nx > max_n || visited[turn % 2][nx])
+                    continue;
+                visited[turn % 2][nx] = visited[(turn + 1) % 2][x] + 1;
+                if (nx == b)
+                {
+                    ok = 1;
+                    break;
+                }
+                q.push(nx);
+            }
+            if (ok)
+                break;
+        }
+        if (ok)
+            break;
+        turn++;
+    }
+    if (ok)
+        cout << turn << "\n";
+    else
+        cout << -1 << "\n";
     return 0;
-} 
+}
