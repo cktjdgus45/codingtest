3c7ddaaa5a46d7de0ecfb01b02770d14b4929212
pooh <cktjdgus45@naver.com>
2023-06-01 11:28:15 +0900

    14497: 주난의 난

--- app.cpp
+++ app.cpp
@@ -1,58 +1,45 @@
-#include <bits/stdc++.h>
-using namespace std;
-const int max_n = 500000;
-int visited[2][max_n + 4], a, b, ok, turn = 1;
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> a >> b;
-    if (a == b)
-    {
-        cout << 0 << "\n";
-        return 0;
-    }
-    queue<int> q;
-    visited[0][a] = 1;
-    q.push(a);
-    while (q.size())
-    {
-        b += turn;
-        if (b > max_n)
-            break;
-        if (visited[turn % 2][b])
-        {
-            ok = true;
-            break;
-        }
-        int qSize = q.size();
-        for (int i = 0; i < qSize; i++)
-        {
-            int x = q.front();
-            q.pop();
-            for (int nx : {x + 1, x - 1, x * 2})
-            {
-                if (nx < 0 || nx > max_n || visited[turn % 2][nx])
-                    continue;
-                visited[turn % 2][nx] = visited[(turn + 1) % 2][x] + 1;
-                if (nx == b)
-                {
-                    ok = 1;
-                    break;
+#include <stdio.h>
+#include<algorithm>
+#include<queue>
+using namespace std; 
+char a[301][301];
+int n, m, x1, y1, x2, y2; 
+typedef pair<int, int> pii;
+int visited[301][301];
+const int dy[4] = {-1, 0, 1, 0};
+const int dx[4] = {0, 1, 0, -1};
+int ret; 
+queue<int> q;
+int main(){
+    scanf("%d %d", &n, &m);
+    scanf("%d %d %d %d", &y1, &x1, &y2, &x2);
+    y1--, x1--, y2--, x2--; 
+    for(int i = 0; i < n; i++){
+        scanf("%s", a[i]); 
+    }  
+    q.push(1000 * y1 + x1);
+    visited[y1][x1] = 1; 
+    int cnt = 0; 
+    while(a[y2][x2] != '0'){ 
+        cnt++;
+        queue<int> temp; 
+        while(q.size()){
+            int y = q.front() / 1000; 
+            int x = q.front() % 1000;  
+            q.pop();  
+            for(int i = 0; i < 4; i++){
+                int ny = y + dy[i]; 
+                int nx = x + dx[i];
+                if(ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx]) continue; 
+                visited[ny][nx] = cnt;  
+                if(a[ny][nx] != '0'){
+                    a[ny][nx] = '0'; 
+                    temp.push(1000 * ny + nx);
                 }
-                q.push(nx);
+                else q.push(1000 * ny + nx); 
             }
-            if (ok)
-                break;
         }
-        if (ok)
-            break;
-        turn++;
+        q = temp;  
     }
-    if (ok)
-        cout << turn << "\n";
-    else
-        cout << -1 << "\n";
-    return 0;
+    printf("%d\n", visited[y2][x2]); 
 }
