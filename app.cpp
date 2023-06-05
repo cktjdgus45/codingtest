3834d46df8aa1686125875372bcae79245cc5f36
pooh <cktjdgus45@naver.com>
2023-06-05 12:02:40 +0900

    19942 : 다이어트

--- app.cpp
+++ app.cpp
@@ -1,45 +1,47 @@
-#include <stdio.h>
-#include<algorithm>
-#include<queue>
-using namespace std; 
-char a[301][301];
-int n, m, x1, y1, x2, y2; 
-typedef pair<int, int> pii;
-int visited[301][301];
-const int dy[4] = {-1, 0, 1, 0};
-const int dx[4] = {0, 1, 0, -1};
-int ret; 
-queue<int> q;
+#include<bits/stdc++.h>
+using namespace std;  
+typedef long long ll; 
+const int INF = 987654321;
+int n, mp, mf, ms, mv;
+int b, c, d, e, ret = INF, sum;
+struct A{
+	int mp, mf, ms, mv, cost; 
+}a[16]; 
+map<int, vector<vector<int>>> ret_v;
 int main(){
-    scanf("%d %d", &n, &m);
-    scanf("%d %d %d %d", &y1, &x1, &y2, &x2);
-    y1--, x1--, y2--, x2--; 
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n; 
+    cin >> mp >> mf >> ms >> mv;  
     for(int i = 0; i < n; i++){
-        scanf("%s", a[i]); 
-    }  
-    q.push(1000 * y1 + x1);
-    visited[y1][x1] = 1; 
-    int cnt = 0; 
-    while(a[y2][x2] != '0'){ 
-        cnt++;
-        queue<int> temp; 
-        while(q.size()){
-            int y = q.front() / 1000; 
-            int x = q.front() % 1000;  
-            q.pop();  
-            for(int i = 0; i < 4; i++){
-                int ny = y + dy[i]; 
-                int nx = x + dx[i];
-                if(ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx]) continue; 
-                visited[ny][nx] = cnt;  
-                if(a[ny][nx] != '0'){
-                    a[ny][nx] = '0'; 
-                    temp.push(1000 * ny + nx);
-                }
-                else q.push(1000 * ny + nx); 
-            }
-        }
-        q = temp;  
-    }
-    printf("%d\n", visited[y2][x2]); 
+    	cin >> a[i].mp >> a[i].mf >> a[i].ms >> a[i].mv >> a[i].cost;  
+	}
+	for(int i = 1; i < (1 << n); i++){
+		b = c = d = e = sum = 0;
+        vector<int> v; 
+		for(int j = 0; j < n; j++){
+			if(i & (1 << j)){
+                v.push_back(j + 1);
+				b += a[j].mp;
+				c += a[j].mf;
+				d += a[j].ms;
+				e += a[j].mv;
+				sum += a[j].cost;
+			}
+		}
+		if(b >= mp && c >= mf && d >= ms && e >= mv){
+			if(ret >= sum){
+				ret = sum;
+                ret_v[ret].push_back(v); 
+			}
+		} 
+	}  
+	if(ret == INF) cout << -1 << '\n';
+	else{
+        sort(ret_v[ret].begin(), ret_v[ret].end());  
+		cout << ret << "\n";
+		for(int a : ret_v[ret][0]){
+			cout << a << " ";
+		} 
+	}  
+	return 0; 
 }
