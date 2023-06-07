7841926a5dfd2c0bc6eff748bae3ae3c995a259e
pooh <cktjdgus45@naver.com>
2023-06-07 16:16:09 +0900

    5430 AC

--- app.cpp
+++ app.cpp
@@ -1,16 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, x;
-char s[11];
-int main() {
-    scanf(" %d", &m);
-    for (int i = 0; i < m; i++) {
-        scanf(" %s %d", &s, &x); 
-        if (s[0]=='a' && s[1]=='d') n |= (1 << x);
-        else if (s[0] == 'r') n &= ~(1 << x);
-        else if (s[0] == 'c') printf("%d\n", (n&(1 << x)) ==0? 0: 1);
-        else if (s[0] == 't') n ^=(1<<x);
-        else if (s[0] == 'a' && s[1] == 'l') n = (1 << 21) - 1;
-        else n = 0;
+int T, N, x;
+string P, order;
+int main(){ 
+	cin >> T;  
+    for(int t = 0; t < T; t++){ 
+        deque<int> D;
+        cin >> P >> N >> order;
+        x = 0;
+        for(char c : order){
+        	if(c == '[' || c == ']') continue;
+        	// 숫자가 나오면 현재 수*10 한 뒤 더함
+            if(c >= '0' && c <= '9') x = x*10 + c-'0';
+            // 아닐 경우 현재 수를 덱에 넣음
+            else{
+                if(x > 0) D.push_back(x);
+                x = 0; 
+            }
+		} 
+		if(x > 0) D.push_back(x);
+        // 초기에는 에러 없음, 뒤집히지 않은 상태
+        bool error = false, rev = false;
+ 		for(char a : P){
+ 			if(a == 'R') rev = !rev; 
+ 			else{
+                // 비어있는데 제거하려 하면 에러
+                if(D.empty()){
+                    error = true;
+                    break;
+                }
+                if(rev) D.pop_back();
+                else D.pop_front(); 
+			}
+		}  
+        // 에러가 발생한 경우
+        if(error) cout << "error" << '\n';
+        // 아닐 경우 덱의 원소를 하나하나 출력
+        else{
+        	cout << "["; 
+            // 덱이 뒤집힌 상태면 진짜로 뒤집어 준다.
+            if(rev) reverse(D.begin(), D.end());
+            for(int i = 0; i < D.size(); i++){
+        		cout << D[i];  
+                if(i < D.size()-1) cout << ",";
+            }
+        	cout << "]\n";  
+        }
     }
-}
+} 
