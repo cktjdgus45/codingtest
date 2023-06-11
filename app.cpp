57e4244eaacf42a2cbd82f404d478e3b356ecea4
pooh <cktjdgus45@naver.com>
2023-06-11 18:42:22 +0900

    2109 : 순회강연

--- app.cpp
+++ app.cpp
@@ -1,35 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-string string_add(string a, string b)
-{
-	int sum = 0;
-	string ret;
-	while (a.size() || b.size() || sum)
-	{
-		if (a.size())
-		{
-			cout << a.back() << "\n";
-			sum += a.back() - '0';
-			a.pop_back();
-		}
-		if (b.size())
-		{
-			sum += b.back() - '0';
-			b.pop_back();
-		}
-		ret += (sum % 10) + '0';
-		sum /= 10;
-	}
-	reverse(ret.begin(), ret.end());
-	return ret;
-}
-string a, b;
+int n, a, b, ret;
+vector<pair<int, int>> v;
+priority_queue<int, vector<int>, greater<int>> pq;
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> a >> b;
-	cout << string_add(a, b) << '\n';
-	return 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+		v.push_back({b, a});
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < n; i++)
+	{
+		pq.push(v[i].second);
+		if (pq.size() > v[i].first)
+		{
+			pq.pop();
+		}
+	}
+	while (pq.size())
+	{
+		ret += pq.top();
+		pq.pop();
+	}
+	cout << ret << "\n";
 }
