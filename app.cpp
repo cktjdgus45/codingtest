117ae0b68f56e95769ae35a55905c777e05bdc13
pooh <cktjdgus45@naver.com>
2023-07-21 14:24:02 +0900

    2468-안전 영역

--- app.cpp
+++ app.cpp
@@ -1,25 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-int dp[1004][2][34], n, m, b[1004];
-
-int go(int idx, int tree, int cnt)
+int a[101][101], visited[101][101], n, ret = 1;
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
+void dfs(int y, int x, int d)
 {
-    if (cnt < 0)
-        return -1e9;
-    if (idx == n)
-        return 0;
-    int &ret = dp[idx][tree][cnt];
-    if (~ret)
-        return ret;
-    return ret = max(go(idx + 1, tree ^ 1, cnt - 1), go(idx + 1, tree, cnt)) + (tree == b[idx] - 1); // 부분최적해합->global합. ,그대로있거나,움직이거나
+    visited[y][x] = 1;
+    for (int i = 0; i < 4; i++)
+    {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (ny < 0 || nx < 0 || ny >= n || nx >= n)
+            continue;
+        if (!visited[ny][nx] && a[ny][nx] > d)
+            dfs(ny, nx, d);
+    }
+    return;
 }
 
 int main()
 {
-    memset(dp, -1, sizeof(dp));
-    cin >> n >> m;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
     for (int i = 0; i < n; i++)
-        cin >> b[i]; // 자두떨어질 나무 순서도.
-    cout << max(go(0, 1, m - 1), go(0, 0, m)) << "\n";
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    for (int d = 1; d < 101; d++)
+    {
+        fill(&visited[0][0], &visited[0][0] + 101 * 101, 0);
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (a[i][j] > d && !visited[i][j])
+                {
+                    dfs(i, j, d);
+                    cnt++;
+                }
+            }
+        }
+        ret = max(ret, cnt);
+    }
+    cout << ret << '\n';
     return 0;
 }
