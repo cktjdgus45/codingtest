a46c9ed5ea4610ecfd443b2961cdb9b3af792164
pooh <cktjdgus45@naver.com>
2023-07-27 17:47:19 +0900

    dfs

--- app.cpp
+++ app.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int a[101][101], visited[101][101], n, ret = 1;
-int dy[4] = {-1, 0, 1, 0};
-int dx[4] = {0, 1, 0, -1};
+int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
 void dfs(int y, int x, int d)
 {
     visited[y][x] = 1;
@@ -10,12 +9,7 @@ void dfs(int y, int x, int d)
     {
         int ny = y + dy[i];
         int nx = x + dx[i];
-        if (ny < 0 || nx < 0 || ny >= n || nx >= n)
-            continue;
-        if (!visited[ny][nx] && a[ny][nx] > d)
-            dfs(ny, nx, d);
     }
-    return;
 }
 
 int main()
@@ -26,7 +20,7 @@ int main()
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int i = 0; j < n; j++)
         {
             cin >> a[i][j];
         }
@@ -46,8 +40,8 @@ int main()
                 }
             }
         }
-        ret = max(ret, cnt);
     }
+    ret = max(ret, cnt);
     cout << ret << '\n';
     return 0;
 }
