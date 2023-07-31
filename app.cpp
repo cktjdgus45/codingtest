504d471388cb81175ae929ba9d17be89385f21d4
pooh <cktjdgus45@naver.com>
2023-07-31 15:43:49 +0900

    2828사과담기

--- app.cpp
+++ app.cpp
@@ -1,47 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[101][101], visited[101][101], n, ret = 1;
-int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
-void dfs(int y, int x, int d)
-{
-    visited[y][x] = 1;
-    for (int i = 0; i < 4; i++)
-    {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-    }
-}
-
+int n, m, j, l, r, temp, ret;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> n;
-    for (int i = 0; i < n; i++)
+    cin >> n >> m >> j;
+    l = 1;
+    for (int i = 0; i < j; i++)
     {
-        for (int i = 0; j < n; j++)
+        r = l + m - 1;
+        cin >> temp;
+        if (temp >= l && temp <= r)
+            continue;
+        else
         {
-            cin >> a[i][j];
-        }
-    }
-    for (int d = 1; d < 101; d++)
-    {
-        fill(&visited[0][0], &visited[0][0] + 101 * 101, 0);
-        int cnt = 0;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
+            if (temp < l)
             {
-                if (a[i][j] > d && !visited[i][j])
-                {
-                    dfs(i, j, d);
-                    cnt++;
-                }
+                ret += (l - temp);
+                l = temp;
+            }
+            else
+            {
+                l += (temp - r);
+                ret += (temp - r);
             }
         }
     }
-    ret = max(ret, cnt);
-    cout << ret << '\n';
+    cout << ret << "\n";
     return 0;
 }
