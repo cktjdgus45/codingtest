74b0cad63a060a938e282645626a5512570f9149
pooh <cktjdgus45@naver.com>
2023-08-02 15:43:48 +0900

    dp

--- app.cpp
+++ app.cpp
@@ -1,23 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int n = 4;
-string a[4] = {"사과", "딸기", "포도", "배"};
-void go(int num)
+#define MAX_N 16;
+int n, dp[MAX_N][1 << MAX_N], dist[MAX_N][MAX_N];
+int tsp(int here, int visited)
 {
-    string ret = "";
+    if (visited == ((1 << n) - 1))
+        return dist[here][0] ? dist[here][0] : INF;
+    int &ret = dp[here][visited];
+    if (ret != -1)
+        return ret;
+    ret = INF;
     for (int i = 0; i < n; i++)
     {
-        if (num & (1 << i))
-            ret += a[i] + " ";
+        if (visited & (1 << i))
+            continue;
+        if (dist[here][i] == 0)
+            continue;
+        ret = min(ret, tsp(i, visited | (1 << i)) + dist[here][i]);
     }
-    cout << ret << "\n";
-    return;
+    return ret;
 }
 int main()
 {
-    for (int i = 1; i < n; i++)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+    for (int i; i < n; i++)
     {
-        go(1 | (1 << i));
+        for (int j; j < n; j++)
+        {
+            cin >> dist[i][j];
+        }
     }
+    memset(dp, -1, sizeof(dp));
+    cout << tsp(0, 1) << "\n";
     return 0;
 }
