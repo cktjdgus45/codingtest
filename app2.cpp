db596935641dcb9ef08e303f86332a812a8eee0f
pooh <cktjdgus45@naver.com>
2023-08-15 14:13:01 +0900

    line swipping

--- app2.cpp
+++ app2.cpp
@@ -1,19 +1,39 @@
 #include <cstdio>
+#include <cstring>
 #include <algorithm>
-#include <vector>
+#include <iostream>
+#include <unordered_map>
 using namespace std;
-int n, sum, a, ret = -1004;
+typedef pair<int, int> P;
+P L[1000004];
+int n, from, to, l, r, ret;
 int main()
 {
-    scanf("%d", &n);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", &a);
-        sum += a;
-        ret = max(ret, a);
-        if (sum < 0)
-            sum = 0;
+        cin >> from >> to;
+        L[i] = P(from, to);
     }
-    printf("%d\n", ret);
-    return 0;
+    sort(L, L + n); // sort
+    l = L[0].first;
+    r = L[0].second;
+    for (int i = 1; i < n; i++) // logic
+    {
+        if (r < L[i].first)
+        {
+            ret += (r - l);
+            r = L[i].first;
+            l = L[i].second;
+        }
+        else if (r <= L[i].second && r >= L[i].first)
+        {
+            r = L[i].second;
+        }
+    }
+    ret += (r - l);
+    cout << ret << "\n";
 }
