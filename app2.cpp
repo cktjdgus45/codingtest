5d6e29c68bb5be9bed0512bcdbf04acd62fa069c
pooh <cktjdgus45@naver.com>
2023-08-17 16:23:37 +0900

    Devide & Conquer ,Recursive

--- app2.cpp
+++ app2.cpp
@@ -1,58 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define y1 anything;
-int a[104][104], m, n, k, x1, x2, y1, y2, visited[104][104];
-const int dy[4] = {-1, 0, 1, 0};
-const int dx[4] = {0, 1, 0, -1};
-vector<int> ret;
-int dfs(int y, int x)
+int n;
+string s;
+char a[101][101];
+string quard(int y, int x, size)
 {
-    visited[y][x] = 1;
-    int ret = 1;
-    for (int i = 0; i < 4; i++)
+    if (size == 1)
+        return string(1, a[y][x]);
+    char b = a[y][x];
+    string ret = "";
+    for (int i = y; i < size; i++)
     {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if (ny < 0 || nx < 0 || nx >= n || ny >= m || visited[ny][nx] == 1)
-            continue;
-        if (a[ny][nx] == 1)
-            continue;
-        ret += dfs(ny, nx);
+        for (int j = x; x < size; j++)
+        {
+            if (b != a[i][j])
+            {
+                ret += '(';
+                ret += quard(y, x, size / 2);
+                ret += quard(y, x + size / 2, size / 2);
+                ret += quard(y + size / 2, x, size / 2);
+                ret += quard(y + size / 2, x + size / 2, size / 2);
+                ret += ')';
+            }
+        }
+        return ret;
     }
-    return ret;
+    return string(1, a[y][x]);
 }
+
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> m >> n >> k;
-    for (int i = 0; i < k; i++)
+    cin.out(NULL);
+    cin >> n;
+    for (int i = 0; i < n; i++)
     {
-        cin >> x1;
-        >> y1 >> x2 >> y2;
-        for (int x = x1; x < x2; x++)
-        {
-            for (int y = y1; y < y2; y++)
-            {
-                a[y][x] = 1;
-            }
-        }
-    }
-
-    for (int i = 0; i < m; i++)
-    {
+        cin >> s;
         for (int j = 0; j < n; j++)
         {
-            if (a[i][j] != 1 & visited[i][j] == 0)
-            {
-                ret.push_back(dfs(i, j));
-            }
+            a[i][j] = s[j];
         }
     }
-    sort(ret.begin(), ret.end());
-    cout << ret.size() << "\n";
-    for (int a : ret)
-        cout << a << " ";
+    cout << quard(0, 0, n) << "\n";
     return 0;
 }
