c2a2e49eb702467ed6707e82b992250a8374f6dc
pooh <cktjdgus45@naver.com>
2023-08-22 15:07:18 +0900

    완탐

--- app2.cpp
+++ app2.cpp
@@ -1,41 +1,16 @@
-#define prev cha;
-int n, o, A, B, asum, bsum;
-string s, prev;
-string print(int a)
-{
-    string d = "00" + to_string(a / 60);
-    string e = "00" + to_string(a % 60);
-    return d.substr(d.size() - 2, 2) + ":" + e.substr(e.size() - 2, 2);
-}
-int changeToInt(string a)
-{
-    return atoi(a.substr(0, 2).c_str()) * 60 + atoi(a.substr(3, 2).c_str());
-}
-void go(int &sum, string s)
-{
-    sum += (changeToInt(s) - changeToInt(prev));
-}
+#include <bits/stdc++.h>
+using namespace std;
+int n;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
     cin >> n;
-    for (int i = 0; i < n; i++)
+    int i = 666;
+    for (;; i++)
     {
-        cin >> o >> s;
-        if (A > B)
-            go(asum, s);
-        else if (A < B)
-            go(bsum, s);
-        o == 1 ? A++ : B++;
-        prev = s;
+        if (to_string(i).find("666") != string::npos)
+            n--; // 666 이 있다면 n--; i++하면서.
+        if (n == 0)
+            break;
     }
-    if (A > B)
-        go(asum, "48:00");
-    else if (A < B)
-        go(bsum, "48:00");
-    cout << print(asum) << "\n";
-    cout << print(bsum) << "\n";
-    return 0;
+    cout << i << "\n";
 }
