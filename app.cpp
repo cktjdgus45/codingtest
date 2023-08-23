a004db6f9403506ba54ace04806abd557d382656
pooh <cktjdgus45@naver.com>
2023-08-23 14:31:06 +0900

    짝짓기-스택

--- app.cpp
+++ app.cpp
@@ -1,35 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-ll n, k, ret, temp1, temp;
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> n >> k;
-    vector<pair<ll, ll>> v(n); // v(n) , v(2)
-    vector<ll> vv(k);          // vv(k) , vv(1)
-    for (int i = 0; i < n; i++)
+    while (true)
     {
-        cin >> v[i].first >> v[i].second;
-    }
-    for (int i = 0; i < k; i++)
-        cin >> vv[i];
-    sort(v.begin(), v.end());
-    sort(vv.begin(), vv.end());
-    priority_queue<ll> pq;
-    int j = 0;
-    for (int i = 0; i < k; i++)
-    {
-        while (j < n && v[j].first <= vv[i])
-            pq.push(v[j++].second);
-        if (pq.size())
+        string s;
+        getline(cin, s);
+        if (s == '.')
+            break;
+        stack<int> stk;
+        bool check = true;
+        for (int i = 0; i < s.length(); i++)
         {
-            ret += pq.top();
-            pq.pop();
+            if (s[i] == ')')
+            {
+                if (stk.size() == 0 || stk.top == '[')
+                {
+                    check = false;
+                    break;
+                }
+            }
+            else
+            {
+                stk.pop();
+            }
+            if (s[i] == ']')
+            {
+                if (stk.size() == 0 || stk.top == '(')
+                    check = false;
+                break;
+            }
+            else
+            {
+                stk.pop();
+            }
+            if (s[i] == '(')
+                stk.push(s[i]);
+            if (s[i] == '[')
+                stk.push(s[i]);
         }
+        if (check && stk.size() == 0)
+            cout << "yes\n";
+        else
+            cout << "no\n";
     }
-    cout << ret << "\n";
     return 0;
 }
