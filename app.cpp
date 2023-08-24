b564eb8849750c42b04f471a4977b4a5a30a7e49
pooh <cktjdgus45@naver.com>
2023-08-24 13:48:44 +0900

    DFS

--- app.cpp
+++ app.cpp
@@ -1,80 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[10][10], visited[10][10], n, m, ret;
-vector<pair<int, int>> virusList, wallList;
-
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1};
-
-// dfs
-void dfs(int y, int x)
+int a[104][104], visited[104][104];
+int dy[] = {-1, 0, 1, 0}, dx[] = {0, 1, 0, -1};
+int n, m, cnt, cnt2;
+vector<pair<int, int>> v;
+void go(iny y, int x)
 {
+    visited[y][x] = 1;
+    if (a[y][x] == 1)
+    {
+        v.push_back({y, x});
+        return;
+    }
     for (int i = 0; i < 4; i++)
     {
         int ny = y + dy[i];
         int nx = x + dx[i];
-        if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx] || a[ny][nx] == 1)
+        if (ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx])
             continue;
-        visited[ny][nx] = 1;
-        dfs(ny, nx);
+        go(ny, nx);
     }
-}
-
-int solve()
-{
-    // virus set-up
-    fill(&visited[0][0], &visited[0][0] + 10 * 10, 0);
-    for (pair<int, int> b : virusList)
-    {
-        visited[b.first][b.second] = 1;
-        dfs(b.first, b.second);
-    }
-    // brute force , counting
-    int cnt = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (a[i][j] == 0 && !visited[i][j])
-                cnt++;
-        }
-    }
-    return cnt;
+    return;
 }
 
 int main()
 {
     // set-up
-    cin >> m >> n;
+    cin >> n >> m;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; m++)
+        for (int j = 0; j < m; j++)
         {
             cin >> a[i][j];
-            if (a[i][j] == 2)
-                virusList.push_back({i, j});
-            if (a[i][j] == 0)
-                wallList.push_back({i, j});
         }
     }
-
-    // combination
-    for (int i = 0; i < wallList.size(); i++)
+    while (true)
     {
-        for (int j = 0; j < i; j++)
+        cnt2 = 0;
+        fill(&visited[0][0], &visited[0][0] + 104 * 104, 0);
+        v.clear();
+        go(0, 0);
+        for (pair<int, int> b : v)
         {
-            for (int k = 0; j < j; k++)
+            cnt2++;
+            a[b.first][b.second] = 0;
+        }
+        bool flag = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
             {
-                a[wallList[i].first][wallList[i].second] = 1;
-                a[wallList[i].first][wallList[i].second] = 1;
-                a[wallList[j].first][wallList[j].second] = 1;
-                ret = max(ret, solve());
-                a[wallList[j].first][wallList[j].second] = 0;
-                a[wallList[k].first][wallList[k].second] = 0;
-                a[wallList[k].first][wallList[k].second] = 0;
+                if (a[i][j] != 0)
+                    flag = 1;
             }
         }
+        cnt++;
+        if (!flag)
+            break;
     }
-    cout << ret << "\n";
-    return 0;
+    cout << cnt << cnt2 << "\n";
+    return;
 }
