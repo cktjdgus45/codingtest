380df0033ef76a34dd675a88bb7e10563c208021
pooh <cktjdgus45@naver.com>
2023-08-28 20:28:03 +0900

    Dijkstra

--- app.cpp
+++ app.cpp
@@ -1,41 +1,51 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <queue>
 using namespace std;
-
-vector<int> adj[10001];
-int dp[10001], mx, visited[10001], n, m, a, b;
-
-int dfs(int here)
-{
-    visited[here] = 1;
-    int ret = 1;
-    for (int there : adj[here])
-    {
-        if (visited[there])
-            continue;
-        ret += dfs(there);
-    }
-    return ret;
-}
-
+int V, E, K, u, v, w;
+vector<pair<int, int>> adj[20001];
+int dist[20001];
+const int INF = 987654321;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> n >> m;
-    while (m--)
-    {
-        cin >> a >> b;
-        adj[b].push_back(a);
-    }
-    for (int i = 0; i < n; i++)
-    {
-        memset(visited, 0, sizeof(visited));
-        dp[i] = dfs(i);
-        mx = max(dp[i], mx);
-    }
-    for (int i = 1; i <= n; i++)
-        if (mx == dp[i])
-            cout << i << " ";
-    return 0;
+	// set-up
+	scanf("%d %d %d", &V, &E, &K);
+	fill(dist, dist + 20001, INF);
+	for (int i = 0; i < E; i++)
+	{
+		scanf("%d %d %d", &u, &v, &w);
+		adj[u].push_back(make_pair(w, v));
+	}
+	pq.push(make_pair(0, K)); // adj (w,v) == pt (w.v)
+	dist[K] = 0;			  // dist[V] = w(E);
+
+	// Dijkstra
+	while (pq.size())
+	{
+		int here = pq.top().second;
+		int here_dist = pq.top().first;
+		pq.pop();
+		if (dist[here] != here_dist)
+			continue;
+		for (pair<int, int> there : adj[here])
+		{
+			int _there = there.second;
+			int _there_dist = there.first;
+			if (dist[_there] > dist[here] + _there_dist)
+			{
+				dist[_there] = dist[here] + _there_dist;
+				pq.push(dist[_there], _there);
+			}
+		}
+	}
+	for (int i = 1; i <= V; i++)
+	{
+		if (dist[i] == INF)
+			pust("INF");
+		else
+			printf("%d", dist[i]);
+	}
+	return 0;
 }
