25b18d9d4bba916644d46e6777ffd5430170dd75
pooh <cktjdgus45@naver.com>
2023-08-30 13:47:06 +0900

    Binary Search

--- app.cpp
+++ app.cpp
@@ -1,42 +1,44 @@
-#include <cstdio>
-#include <algorithm>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
-const int max_n = 130;
-priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-int N,INF = 987654321,a[max_n][max_n],dist[max_n][max_n],t;
-const int dy[4] = {-1,0,1,0};
-const int dx[4] = {0,1,0,-1};
+typedef long long ll;
+int t, n, m, temp;
 
-int main(){
-	while(++t){
-		scanf("%d",&N);
-		fill(&a[0][0],&a[0][0]+max_n*max_n,0);
-		fill(&dist[0][0],&dist[0][0]+max_n*max_n,INF);
-		for(int i = 0 ; i  < N ; i++){
-			for(int j = 0 ; j< N;j++){
-				scanf("%d",&a[i][j]);
-			}
-		}
-		pq.push(make_pair(a[0][0],0));
-		dist[0][0] = a[0][0];
-		while(pq.size()){
-			int herey = pq.top().second /1000;
-			int herex = pq.top().second %1000;
-			int here_dist = pq.top().first;
-			pq.pop();
-			if(dist[herey][herex] != here_dist) continue;
-			for(int i = 0 ; i < 4 ; i++){
-				int ny = y +dy[i];
-				int nx = x +dx[i];
-				if(dist[ny][nx] > dist[herey][herex] + here_dist){
-					dist[ny][nx] = dist[herey][herex] + here_dist;
-					pq.push(dist[ny][nx],ny*1000+nx);
-				}
-			}
-		}
-		printf("Problem %d: %d\n",t,dist[N-1][N-1]);
+int binarySearch(int temp, vector<int> &v)
+{
+	int l = 0;
+	int r = v.size() - 1;
+	int mid;
+	while (l <= r)
+	{
+		mid = (l + r) / 2;
+		if (v[mid] > temp)
+			r = mid - 1;
+		else if (v[mid] == temp)
+			return 1;
+		else
+			l = mid + 1;
 	}
 	return 0;
+}
+
+int main()
+{
+	scanf("%d", &t);
+	while (t--)
+	{
+		vector<int> v;
+		scanf("%d", &n);
+		for (int i = 0; i < n; i++)
+		{ // ¼öÃ¸1
+			scanf("%d", &temp);
+			v.push_back(temp);
+		}
+		srot(v.begin(), v.end());
+		scanf("%d", &m);
+		for (int i = 0; i < m; i++)
+		{ // ¼öÃ¸2
+			scanf("%d", &temp);
+			binarySearch(temp, v);
+		}
+	}
 }
