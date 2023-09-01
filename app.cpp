68c74c4d6bfa01aa31b0ff7078effedd57862d4c
pooh <cktjdgus45@naver.com>
2023-09-01 18:33:31 +0900

    Greedy Algorythm

--- app.cpp
+++ app.cpp
@@ -1,30 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, t;
+vector<pair<int, int>> v;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	cin >> t;
-	while (t--)
+	cin >> n;
+	for (int i = 0; i < n; i++)
 	{
-		int ret = 0;
-		cin >> n >> m;
-		vector<int> a(n), b(m);
-		for (int i = 0; i < n; i++)
-			cin >> a[i];
-		for (int i = 0; i < m; i++)
-			cin >> b[i];
-		sort(a.begin(), a.end());
-		sort(b.begin(), b.end());
-		for (int i = 0; i < n; i++)
-		{
-			auto pos = lower_bound(b.begin(), b.end(), a[i]);
-			ret += (int)(pos - b.begin());
-		}
-		cout << ret << "\n";
+		cin >> from >> to;
+		v.push_back({to, from});
 	}
+	sort(v.begin(), v.end());
+	from = v[0].second;
+	to = v[0].first;
+	for (int i = 1; i < n; i++)
+	{
+		if (v[i].second < to)
+			continue;
+		to = v[i].first;
+		from = v[i].second;
+		ret++;
+	}
+	cout << ret << "\n";
+	return 0;
 }
