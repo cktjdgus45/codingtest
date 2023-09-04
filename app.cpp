ffedc1d7ee7c005cd91aa1ba8be35fe6c721cdce
pooh <cktjdgus45@naver.com>
2023-09-04 10:37:22 +0900

    greedy , string ,stack

--- app.cpp
+++ app.cpp
@@ -1,17 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 string S, T, ret;
+// stack<char> stk;
 int main()
 {
 	cin >> S >> T;
 	for (char a : S)
 	{
-		ret += a;
+		ret += a; // stk.push(a);
 		if (ret.size() >= T.size() && ret.substr(ret.size() - T.size(), T.size()) == T)
+		// if(stk.size()>=T.size()&&stk.top()==T[T.size()-1])
+		//{
+		//  string ss =""; ss+= stk.top(); stk.pop();
+		// }
+		// reverse(ss.begin(),ss.end());
+		// if(T!=ss)
+		//{
+		// for(int i : ss){ stk.push(i)};
+		//}
 		{
 			ret.erase(ret.end() - T.size(), ret.end());
 		}
 	}
+	// if(stk.size()==0){cout <<"FRULA"} else{ while(stk.size()) ret+=stk.top(); stk.pop();} reverse(ret.begin(); ret.end()); cout << ret<<"\n";
 	if (!ret.size())
 		cout << "FRULA"
 			 << "\n";
