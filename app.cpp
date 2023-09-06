c069ebc4852c576ae54e98a3cb7ad7ff3eeb817d
pooh <cktjdgus45@naver.com>
2023-09-06 19:57:44 +0900

    teratosneche - prime number

--- app.cpp
+++ app.cpp
@@ -1,38 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-ll n, k, ret;
+
+const int max_n = 40;
+bool che[max_n + 1];
+
+vector<int> era(int max_n)
+{
+	vector<int> v;
+	for (int i = 2; i <= max_n; i++)
+	{
+		if (che[i])
+			continue;
+		for (int j = 2 * i; j <= max_n; j += i)
+		{
+			cout << j << "\n ";
+			che[j] = 1;
+		}
+	}
+	for (int i = 2; i <= max_n; i++)
+	{
+		if (che[i] == 0)
+			v.push_back(i);
+	}
+	return v;
+}
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> n >> k;
-	vector<pair<ll, ll>> v(n);
-	vector<ll> vv(k);
-	for (int i = 0; i < n; i++)
-	{
-		cin >> v[i].first >> v[i].second;
-	}
-	for (int i = 0; i < k; i++)
-	{
-		cin >> vv[i];
-	}
-	sort(v.begin(), v.end());	// 보석 무게,가치
-	sort(vv.begin(), vv.end()); // 가방 최대무게
-	priority_queue<ll> pq;		// 내림차순 , top == max.
-	int j = 0;
-	for (int i = 0; i < k; i++)
-	{
-		while (j < n && v[j].first <= vv[i])
-			pq.push(v[j++].second);
-		if (pq.size())
-		{
-			ret += pq.top();
-			pq.pop();
-		}
-	}
-	cout << ret << "\n";
-	return 0;
+	vector<int> a = era(max_n);
+	for (int i : a)
+		cout << i << " ";
 }
