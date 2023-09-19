bac6b45b86a76c99916b308d734e2d7a735ca1b8
pooh <cktjdgus45@naver.com>
2023-09-19 15:19:34 +0900

    자료구조 , 뱀

--- app.cpp
+++ app.cpp
@@ -1,85 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, ret;
-
-struct Board
-{
-    int a[24][24];
-    void move()
-    {
-        int temp[24][24];
-        for (int i = 0; i < n; i++)
-        {
-            int c = -1, d = 0;
-            for (int j = 0; j < n; j++)
-            {
-                if (a[i][j] == 0)
-                    continue;
-                if (d && a[i][j] == temp[i][c])
-                    temp[i][c] *= 2, d = 0;
-                else
-                    temp[i][++c] = a[i][j], d = 1; // push
-            }
-            for (c++; c < n; c++)
-                temp[i][c] = 0;
-        }
-        memcpy(a, temp, sizeof(a));
-    }
-    void rotate_90()
-    {
-        int temp[24][24];
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                temp[i][j] = a[n - j - 1][i];
-            }
-        }
-        memcpy(a, temp, sizeof(a));
-    }
-    void get_max()
-    {
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                ret = max(ret, a[i][j]);
-            }
-        }
-    }
-};
-
-void go(Board c, int here)
-{
-    if (here == 5)
-    {
-        c.get_max(); // 한쪽방면 max값 축적.
-        return;
-    }
-    for (int i = 0; i < 4; i++)
-    {
-        Board d = c;
-        d.move();        // 한쪽으로 더하기(한쪽방면) ,한쪽으로 더하기(한쪽방면) ,한쪽으로 더하기(한쪽방면),한쪽으로 더하기(한쪽방면) (90도씩 회전후)
-        go(d, here + 1); // here=0 move ,here=1 move , here =2 move ... 총5번 이동.
-        d.rotate_90();   // 회전
-    }
-}
-
+#define time ff
+#define y1 cc
+typedef long long ll;
+int n, k, l, y, x, t, ret, idx, dir = 1;
+int a[104][104], visited[104][104], time;
+char c;
+deque<pair<int, int>> dq;
+vector<pair<int, int>> _time;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, 1, 0, -1};
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> n;
-    Board c;
-    for (int i = 0; i < n; i++)
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n >> k;
+    for (int i = 0; i < k; i++)
     {
-        for (int j = 0; j < n; j++)
+        cin >> y >> x;
+        a[--y][--x] = 1;
+    }
+    cin >> l;
+    for (int i = 0; i < l; i++)
+    {
+        cin >> t >> c;
+        if (c == 'D')
+            _time.push_back({t, 1});
+        else
+            _time.push_back({t, 3});
+    }
+    dq.push_back({0, 0});
+    while (dq.size())
+    {
+        time++;
+        tie(y, x) = dq.front();
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+        if (ny >= n || ny < 0 || nx >= n || nx < 0 || visited[ny][nx])
+            break;
+
+        if (!a[ny][nx])
         {
-            cin >> c.a[i][j];
+            visited[dq.back().first][dq.back().second] = 0;
+            dq.pop_back();
+        }
+        else
+            a[ny][nx] = 0;
+
+        visited[ny][nx] = 1;
+        dq.push_front({ny, nx});
+        if (time == _time[idx].first)
+        {
+            dir = (dir + _time[idx].second) % 4;
+            idx++;
         }
     }
-    go(c, 0);
-    cout << ret << "\n";
+    cout << time << "\n";
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define time ff
+typedef long long ll;
+int n, k, l, y, x, t, ret, idx, dir = 1;
+int a[104][104], visited[104][104], time;
+char c;
+deque<pair<int, int>> dq;
+vector<pair<int, int>> _time;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, 1, 0, -1};
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)
+    { // 사과배열
+        cin >> y >> x;
+        a[--y][--x] = 1;
+    }
+    cin >> l;
+    for (int i = 0; i < l; i++)
+    { // 시간
+        cin >> t >> c;
+        if (c == 'D')
+            _time.push_back({t, 1});
+        else
+            _time.push_back({t, 3});
+    }
+    dq.push_back({0, 0});
+    while (dq.size())
+    {
+        time++;
+        tie(y, x) = dq.front();
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+        if (ny >= n || nx >= n || ny < 0 || nx < 0 || visited[ny][nx])
+            break; // 벽,자신몸
 
-        cout << "\n";
+        if (!a[ny][nx])
+        { // 사과가없다면 꼬리짜르기
+            visited[dq.back().first][dq.back().second] = 0;
+            dq.pop_back();
+        }
+        else
+            a[ny][nx] = 0; // 사과먹음
+
+        visited[ny][nx] = 1;          // 먹든안먹든 이동. visited
+        dq.push_front({ny, nx});      // 좌표 front,머리이동.
+        if (time == _time[idx].first) // time이되면
+        {
+            dir = (dir + _time[idx].second) % 4; // dy,dx 방향돌리기.
+            idx++;
+        }
+    }
+    cout << time << "\n";
+    return 0;
+}
