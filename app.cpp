d228caf220d94f032453dbba0ef2dfff162ba1b2
pooh <cktjdgus45@naver.com>
2023-09-27 16:06:36 +0900

    파이프 옮기기1 dp

--- app.cpp
+++ app.cpp
@@ -1,44 +1,66 @@
 #include <bits/stdc++.h>
-#define MAX_N 16
 using namespace std;
-const int INF = 987654321;
-int n, dp[MAX_N][1 << MAX_N], dist[MAX_N][MAX_N];
-int tsp(int here, int visited)
-{
-    if (visited == (1 << n) - 1) // 기저사례
-    {
-        return dist[here][0] ? dist[here][0] : INF;
-    }
-    int &ret = dp[here][visited]; // 메모이제이션
-    if (ret != -1)
-        return ret;
-    ret = INF;
-    for (int i = 0; i < n; i++) // 로직
-    {
-        if (visited & (1 << i))
-            continue;
-        if (dist[here][i] == 0)
-            continue;
-        ret = min(ret, tsp(i, (visited | (1 << i))) + dist[here][i]);
-        cout << ret << "\n";
-    }
-    return ret;
-}
 
-int main()
+int n, a[24][24], dp[24][24][3];
+
+void fastIO()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> n;
-    for (int i = 0; i < n; i++)
+}
+
+bool check(int y, int x, int d)
+{
+    if (d == 0 || d == 2)
     {
-        for (int j = 0; j < n; j++)
+        if (!a[y][x])
+            return true; // a[y][x] == 0 빈칸
+    }
+    else if (d == 1)
+    {
+        if (a[y][x] == 0 && a[y - 1][x] == 0 && a[y][x - 1] == 0)
+            return true;
+    }
+}
+
+int main()
+{
+    fastIO();
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
         {
-            cin >> dist[i][j];
+            cin >> a[i][j];
         }
     }
-    memset(dp, -1, sizeof(dp)); // 초기화
-    cout << tsp(0, 1) << '\n';
+    dp[1][2][0] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (check(i, j + 1, 0))
+                dp[i][j + 1][0] += dp[i][j][0];
+            if (check(i + 1, j + 1, 1))
+                dp[i + 1][j + 1][1] += dp[i][j][0];
+
+            if (check(i + 1, j, 2))
+                dp[i + 1][j][2] += dp[i][j][2];
+            if (check(i + 1, j + 1, 1))
+                dp[i + 1][j + 1][1] += dp[i][j][2];
+
+            if (check(i, j + 1, 0))
+                dp[i][j + 1][0] += dp[i][j][1];
+            if (check(i + 1, j + 1, 1))
+                dp[i + 1][j + 1][1] += dp[i][j][1];
+            if (check(i + 1, j, 2))
+                dp[i + 1][j][2] += dp[i][j][1];
+        }
+    }
+    int ret = dp[n][n][0];
+    ret += dp[n][n][1];
+    ret += dp[n][n][2];
+    cout << ret << "\n";
     return 0;
 }
