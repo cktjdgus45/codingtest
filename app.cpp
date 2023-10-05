60d4e9a7b77497561ca5d92e9fbb8644de1d6c5b
pooh <cktjdgus45@naver.com>
2023-10-05 18:28:31 +0900

    알약 DP

--- app.cpp
+++ app.cpp
@@ -1,26 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int dp[1004][2][34]; // time,tree,move
-int n, m, b[1004];
 
-int go(int idx, int tree, int cnt)
+typedef long long ll; // 경우의 수 ll박고시작.
+ll n, dp[31][31];     // N <= 30
+
+int go(int whole, int not_whole)
 {
-    if (cnt < 0)
-        return -1e9; // base case
-    if (idx == n)
-        return 0;
-    int &ret = dp[idx][tree][cnt]; // memoization
-    if (~ret)
-        return ret;
-    return ret = max(go(idx + 1, tree ^ 1, cnt - 1), go(idx + 1, tree, cnt)) + (tree == b[idx] - 1);
+    if (whole == 0 && not_whole == 0) // base case
+        return 1;
+    if (dp[whole][not_whole])
+        return dp[whole][not_whole];
+    ll &ret = dp[whole][not_whole]; // memoization
+    if (whole > 0)                  // logic
+        ret += go(whole - 1, not_whole + 1);
+    if (not_whole > 0)
+        ret += go(whole, not_whole - 1);
+    return ret;
 }
 
 int main()
 {
-    memset(dp, -1, sizeof(dp)); // initialize
-    cin >> n >> m;
-    for (int i = 0; i < n; i++)
-        cin >> b[i];
-    cout << max(go(0, 1, m - 1), go(0, 0, m)) << '\n';
-    return 0;
+    while (true)
+    {
+        cin >> n;
+        if (n == 0)
+            break;
+        cout << go(n, 0) << "\n";
+    }
 }
