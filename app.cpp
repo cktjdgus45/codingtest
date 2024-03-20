3c60cd3a2b3532486f78fc75803eb1f704ca6ba0
pooh <cktjdgus45@naver.com>
2024-03-20 15:33:01 +0900

    2559 수열

--- app.cpp
+++ app.cpp
@@ -1,37 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int n;
-string pat, fn, pre, suf;
-
+typedef long long ll;
+int n, k, temp, psum[100001], ret = -10000000;
 int main()
 {
-    cin >> n;
-    cin >> pat;
-    int pos = pat.find('*');
-    pre = pat.substr(0, pos);
-    suf = pat.substr(pos + 1);
-    for (int i = 0; i < n; i++)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++)
     {
-        cin >> fn;
-        if (pre.size() + suf.size() > fn.size())
-        {
-            cout << "NE"
-                 << "n";
-        }
-        else
-        {
-            if (pre == fn.substr(0, pre.size()) && suf == fn.substr(fn.size() - suf.size()))
-            {
-                cout << "DA "
-                     << "\n";
-            }
-            else
-            {
-                cout << "NE "
-                     << "\n";
-            }
-        }
+        cin >> temp;
+        psum[i] = psum[i - 1] + temp;
     }
+    for (int i = k; i <= n; i++)
+    {
+        ret = max(ret, psum[i] - psum[i - k]);
+    }
+    cout << ret << "\n";
     return 0;
 }
