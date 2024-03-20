ba64a4523bf8e009113f6ff1d89d18d29325db85
pooh <cktjdgus45@naver.com>
2024-03-20 13:34:22 +0900

    9996 - 한국이 그리울 땐 서버에 접속하지

--- app.cpp
+++ app.cpp
@@ -1,19 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string s;
+int n;
+string pat, fn, pre, suf;
 
-int main(){
-    getline(cin,s);
-    for(int i = 0 ; i < s.size() ; i++){
-        if(s[i]>=65 && s[i] < 97){
-            if(s[i]+13 >90) s[i]=s[i]+13-26;
-            else s[i] = s[i]+13;
-        }else if(s[i]>=97 && s[i] <=122){
-            if(s[i]+13 >122) s[i] = s[i]+13-26;
-            else s[i] = s[i]+13;
+int main()
+{
+    cin >> n;
+    cin >> pat;
+    int pos = pat.find('*');
+    pre = pat.substr(0, pos);
+    suf = pat.substr(pos + 1);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> fn;
+        if (pre.size() + suf.size() > fn.size())
+        {
+            cout << "NE"
+                 << "n";
         }
-        cout << s[i];
+        else
+        {
+            if (pre == fn.substr(0, pre.size()) && suf == fn.substr(fn.size() - suf.size()))
+            {
+                cout << "DA "
+                     << "\n";
+            }
+            else
+            {
+                cout << "NE "
+                     << "\n";
+            }
+        }
     }
     return 0;
 }
