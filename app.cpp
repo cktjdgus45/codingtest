1742e8a22d30d11ff79e09d8a4fcc5cb4e69ab59
pooh <cktjdgus45@naver.com>
2024-03-21 14:58:01 +0900

    9375 패션왕 신해빈

--- app.cpp
+++ app.cpp
@@ -1,22 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-int n, k, temp, psum[100001], ret = -10000000;
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> n >> k;
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> temp;
-        psum[i] = psum[i - 1] + temp;
+
+int t,n;
+string a,b;
+
+int main(){
+cin >> t;
+while(t--){
+    map<string,int> _map;
+    cin >> n;
+    for(int i = 0 ; i < n;i++){
+        cin >> a >> b ;
+        _map[b]++;
     }
-    for (int i = k; i <= n; i++)
-    {
-        ret = max(ret, psum[i] - psum[i - k]);
+    long long ret = 1;
+    for(auto c:_map){
+        ret *= ((long long)c.second+1);
     }
+    ret--;
     cout << ret << "\n";
-    return 0;
+}
+return 0;
 }
