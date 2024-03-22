51d2d64103498029c6bd83c85a639752f3ffbf65
pooh <cktjdgus45@naver.com>
2024-03-22 09:39:20 +0900

    1213 - 펠린드롬 만들기

--- app.cpp
+++ app.cpp
@@ -1,24 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int t,n;
-string a,b;
-
-int main(){
-cin >> t;
-while(t--){
-    map<string,int> _map;
-    cin >> n;
-    for(int i = 0 ; i < n;i++){
-        cin >> a >> b ;
-        _map[b]++;
+string s, ret;
+int cnt[200], flag;
+char mid;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> s;
+    for (char a : s)
+        cnt[a]++;
+    for (int i = 'Z'; i >= 'A'; i--)
+    {
+        if (cnt[i])
+        {
+            if (cnt[i] & 1)
+            {
+                mid = char(i);
+                flag++;
+                cnt[i]--;
+            }
+            if (flag == 2)
+                break;
+            for (int j = 0; j < cnt[i]; j += 2)
+            {
+                ret = char(i) + ret;
+                ret += char(i);
+            }
+        }
     }
-    long long ret = 1;
-    for(auto c:_map){
-        ret *= ((long long)c.second+1);
-    }
-    ret--;
-    cout << ret << "\n";
-}
-return 0;
+    if (mid)
+        ret.insert(ret.begin() + ret.size() / 2, mid);
+    if (flag == 2)
+        cout << "I'm Sorry Hansoo\n";
+    else
+        cout << ret << "\n";
 }
