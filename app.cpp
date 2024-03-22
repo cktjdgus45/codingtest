f74918f5824cca31492cf5b57c658b0f87885671
pooh <cktjdgus45@naver.com>
2024-03-22 14:28:44 +0900

    3986 - 좋은단어

--- app.cpp
+++ app.cpp
@@ -1,21 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cnt,a[15001],n,m;
-
-int main(){
-    cin >> n >> m;
-    for(int i = 0 ; i < n; i++){
-        cin >> a[i];
-    }
-    if(m > 200000) cout <<0 <<"\n";
-    else {
-        for(int i= 0 ; i < n; i++){
-            for(int j = i+1; j<n; j++){
-                if(a[i]+a[j]==m)cnt++;
+int n, ret;
+string s;
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s;
+        stack<char> stk;
+        for (char a : s)
+        {
+            if (stk.size() && stk.top() == a)
+            {
+                stk.pop();
+            }
+            else
+            {
+                stk.push(a);
             }
         }
-        cout << cnt <<"\n";
+        if (stk.size() == 0)
+            ret++;
     }
+    cout << ret << "\n";
     return 0;
 }
