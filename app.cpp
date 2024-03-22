10f5e0bca870a9b991db8a4d285e484a0b348cb3
pooh <cktjdgus45@naver.com>
2024-03-22 11:26:20 +0900

    1940-주몽

--- app.cpp
+++ app.cpp
@@ -1,38 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-string s, ret;
-int cnt[200], flag;
-char mid;
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cin >> s;
-    for (char a : s)
-        cnt[a]++;
-    for (int i = 'Z'; i >= 'A'; i--)
-    {
-        if (cnt[i])
-        {
-            if (cnt[i] & 1)
-            {
-                mid = char(i);
-                flag++;
-                cnt[i]--;
-            }
-            if (flag == 2)
-                break;
-            for (int j = 0; j < cnt[i]; j += 2)
-            {
-                ret = char(i) + ret;
-                ret += char(i);
+
+int cnt,a[15001],n,m;
+
+int main(){
+    cin >> n >> m;
+    for(int i = 0 ; i < n; i++){
+        cin >> a[i];
+    }
+    if(m > 200000) cout <<0 <<"\n";
+    else {
+        for(int i= 0 ; i < n; i++){
+            for(int j = i+1; j<n; j++){
+                if(a[i]+a[j]==m)cnt++;
             }
         }
+        cout << cnt <<"\n";
     }
-    if (mid)
-        ret.insert(ret.begin() + ret.size() / 2, mid);
-    if (flag == 2)
-        cout << "I'm Sorry Hansoo\n";
-    else
-        cout << ret << "\n";
+    return 0;
 }
