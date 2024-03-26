62db48d0a10947118b262dc1545fba5a87639fb4
pooh <cktjdgus45@naver.com>
2024-03-26 17:21:59 +0900

    2178 - 미로탐색

--- app.cpp
+++ app.cpp
@@ -1,27 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
+const int max_n = 104;
+int n, m, a[max_n][max_n], visited[max_n][max_n], y, x;
+const int dy[4] = {-1, 0, 1, 0};
+const int dx[4] = {0, 1, 0, -1};
 
 int main()
 {
-    while (scanf("%d", &n) != EOF)
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++)
     {
-        int cnt = 1, ret = 1;
-        while (true)
+        for (int j = 0; j < m; j++)
         {
-            if (cnt % n == 0)
-            {
-                printf("%d\n", ret);
-                break;
-            }
-            else
-            {
-                cnt = (cnt * 10) + 1;
-                cnt %= n;
-                ret++;
-            }
+            scanf("%1d", &a[i][j]);
         }
     }
+    queue<pair<int, int>> q;
+    q.push({0, 0});
+    visited[0][0] = 1;
+    while (q.size())
+    {
+        tie(y, x) = q.front();
+        q.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if (ny < 0 || nx < 0 || ny >= n || nx >= m || a[ny][nx] == 0)
+                continue;
+            if (visited[ny][nx])
+                continue;
+            visited[ny][nx] = visited[y][x] + 1;
+            q.push({ny, nx});
+        }
+    }
+    printf("%d", visited[n - 1][m - 1]);
     return 0;
 }
