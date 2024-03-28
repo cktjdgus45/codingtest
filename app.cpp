bf632ef20439ec2f48ccd236cbf57360901a68d0
pooh <cktjdgus45@naver.com>
2024-03-28 14:27:29 +0900

    14502 연구소

--- app.cpp
+++ app.cpp
@@ -1,40 +1,61 @@
-#include<bits/stdc++.h>
-using namespace std;   
-int a[101][101], e[101][101], visited[101][101], n, temp, ret = 1;
-int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
-void dfs(int y, int x, int d){
-    visited[y][x] = 1; 
-    for(int i = 0; i < 4; i++){
-        int ny = y + dy[i]; 
-        int nx = x + dx[i]; 
-        if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue; 
-        if(!visited[ny][nx] && a[ny][nx] > d) dfs(ny, nx, d);
+#include <bits/stdc++.h>
+using namespace std;
+
+const int dy[4] = {-1,0,1,0};
+const int dx[4] = {0,1,0,-1};
+int n ,m,a[10][10],visited[10][10],ret;
+vector<pair<int,int>>virusList,wallList;
+
+void dfs(int y,int x){
+    visited[y][x]=1;
+    for(int i = 0 ; i < 4; i++){
+        int ny = y+dy[i];
+        int nx = x+dx[i];
+        if(ny <0 || nx <0 || ny >=n || nx >=m || visited[ny][nx] || a[ny][nx] ==1) continue;
+        dfs(ny,nx);
     }
     return;
-} 
+}
+
+int solve(){
+    //바이러스 퍼뜨리기
+    fill(&visited[0][0], &visited[0][0] + 10 * 10, 0); 
+    for(pair<int,int>b:virusList){
+        visited[b.first][b.second] =1; //바이러스 표시.
+        dfs(b.first,b.second);
+    }
+    int cnt = 0;        
+    //안전영역 개수 구하기. 
+    for(int i = 0 ; i < n; i++){
+        for(int j = 0 ; j < m; j++){
+            if(a[i][j]==0 && !visited[i][j])cnt++;
+        }
+    }
+    return cnt;
+}
+
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);   
-    cin >> n; 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+    cin >> n >> m;
+    for(int i = 0 ; i < n; i++){
+        for(int j = 0 ; j < m; j++){
             cin >> a[i][j];
+            if(a[i][j]==2)virusList.push_back({i,j});
+            if(a[i][j]==0)wallList.push_back({i,j});
         }
     }
-    for(int d = 1; d < 101; d++){
-        fill(&visited[0][0], &visited[0][0] + 101 * 101, 0); 
-        int cnt = 0;  
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(a[i][j] > d && !visited[i][j]) {
-                    dfs(i, j, d);
-                    cnt++;
-                }
-            }
+    for(int i = 0 ; i < wallList.size();i++){
+        for(int j = 0 ; j < i;j++){
+            for(int k = 0 ; k < j;k++){
+                a[wallList[i].first][wallList[i].second] = 1;
+                a[wallList[j].first][wallList[j].second] = 1;
+                a[wallList[k].first][wallList[k].second] = 1;
+                ret = max(ret,solve());
+                a[wallList[i].first][wallList[i].second] = 0;
+                a[wallList[j].first][wallList[j].second] = 0;
+                a[wallList[k].first][wallList[k].second] = 0;
         }
-        ret = max(ret, cnt);
+        }
     }
-    cout << ret << '\n';
-    return 0; 
+    cout << ret << "\n";
+    return 0;
 }
