3da20a6158b81901c4bb477fb8a0cea0cbc42466
pooh <cktjdgus45@naver.com>
2024-04-04 17:01:27 +0900

    19942 - 다이어트

--- app.cpp
+++ app.cpp
@@ -1,40 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-int R, C, ret, ny, nx, visited[30];
-char a[21][21];
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1};
-void go(int y, int x, int cnt)
+typedef long long ll;
+const int INF = 987654321;
+int n, mp, mf, ms, mv;
+int b, c, d, e, ret = INF, sum;
+struct A
 {
-    ret = max(ret, cnt);
-    for (int i = 0; i < 4; i++)
-    {
-        ny = y + dy[i], nx = x + dx[i];
-        if (ny < 0 || ny >= R || nx < 0 || nx >= C)
-            continue;
-        int next = (int)(a[ny][nx] - 'A');
-
-        if (visited[next] == 0)
-        {
-            visited[next] = 1;
-            go(ny, nx, cnt + 1);
-            visited[next] = 0;
-        }
-    }
-    return;
-}
+	int mp, mf, ms, mv, cost;
+} a[16];
+map<int, vector<vector<int>>> ret_v;
 int main()
 {
-    cin >> R >> C;
-    for (int i = 0; i < R; i++)
-    {
-        for (int j = 0; j < C; j++)
-        {
-            cin >> a[i][j];
-        }
-    }
-    visited[(int)a[0][0] - 'A'] = 1;
-    go(0, 0, 1);
-    cout << ret << '\n';
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n;
+	cin >> mp >> mf >> ms >> mv;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i].mp >> a[i].mf >> a[i].ms >> a[i].mv >> a[i].cost;
+	}
+	for (int i = 1; i < (1 << n); i++)
+	{ // ex i =6 000110 경우의수일때.
+		b = c = d = e = sum = 0;
+		vector<int> v;
+		for (int j = 0; j < n; j++)
+		{ // n(j)번째 영양제가 포함.
+			if (i & (1 << j))
+			{
+				v.push_back(j + 1);
+				b += a[j].mp;
+				c += a[j].mf;
+				d += a[j].ms;
+				e += a[j].mv;
+				sum += a[j].cost;
+			}
+		}
+		if (b >= mp && c >= mf && d >= ms && e >= mv)
+		{
+			if (ret >= sum)
+			{
+				ret = sum;
+				ret_v[ret].push_back(v);
+			}
+		}
+	}
+	if (ret == INF)
+		cout << -1 << '\n';
+	else
+	{
+		sort(ret_v[ret].begin(), ret_v[ret].end());
+		cout << ret << "\n";
+		for (int a : ret_v[ret][0])
+		{
+			cout << a << " ";
+		}
+	}
+	return 0;
 }
