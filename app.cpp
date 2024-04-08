76faf2bdcbd2b7a94568218940e2e5913594c18f
pooh <cktjdgus45@naver.com>
2024-04-08 15:22:23 +0900

    1781 - 컵라면

--- app.cpp
+++ app.cpp
@@ -1,28 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int from, to, n, idx = 0, ret = 1;
+int n, a, b, ret;
+vector<pair<int, int>> v;
+priority_queue<int, vector<int>, greater<int>> pq;
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
     cin >> n;
-    vector<pair<int, int>> v;
     for (int i = 0; i < n; i++)
     {
-        cin >> from >> to;
-        v.push_back({to, from});
+        cin >> a >> b;
+        v.push_back({a, b});
     }
     sort(v.begin(), v.end());
-    from = v[0].second;
-    to = v[0].first;
-    for (int i = 1; i < n; i++)
+    for (int i = 0; i < v.size(); i++)
     {
-        if (v[i].second < to)
-            continue;
-        from = v[i].second;
-        to = v[i].first;
-        ret++;
+        ret += v[i].second;
+        pq.push(v[i].second);
+        if (pq.size() > v[i].first)
+        {
+            ret -= pq.top();
+            pq.pop();
+        }
     }
-    cout << ret << '\n';
+    cout << ret << "\n";
     return 0;
 }
