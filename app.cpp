721800aa209bd1d0ed9816e758e095517b9d2826
pooh <cktjdgus45@naver.com>
2024-04-11 20:29:29 +0900

    2240 - 자두나무

--- app.cpp
+++ app.cpp
@@ -1,54 +1,19 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-ll n, attack, t[130005], a[130005], h[130005], lo, hi, ret;
-void fastIO()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+int dp[1004][2][34], n, m, b[1004];
+
+int go(int idx, int tree, int cnt){
+	if(cnt < 0) return -1e9;
+	if(idx == n) return 0;
+    int &ret = dp[idx][tree][cnt];
+    if(~ret) return ret;  
+    return ret = max(go(idx + 1, tree^1, cnt - 1), go(idx + 1, tree, cnt)) + (tree == b[idx] - 1); 
 }
-bool check(ll mid)
-{
-    ll mxHP = mid;
-    ll init_attack = attack;
-    for (int i = 0; i < n; i++)
-    {
-        if (t[i] == 2)
-        {
-            mid = min(mxHP, mid + h[i]);
-            init_attack += a[i];
-        }
-        else
-        {
-            ll cnt = h[i] / init_attack + (h[i] % init_attack ? 1 : 0);
-            mid -= (cnt - 1) * a[i];
-        }
-        if (mid <= 0)
-            return false;
-    }
-    return true;
-}
-int main()
-{
-    fastIO();
-    cin >> n >> attack;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> t[i] >> a[i] >> h[i];
-    }
-    lo = 1, hi = 1e18 + 4;
-    while (lo <= hi)
-    {
-        ll mid = (lo + hi) / 2;
-        if (check(mid))
-        {
-            hi = mid - 1;
-            ret = mid;
-        }
-        else
-            lo = mid + 1;
-    }
-    cout << ret << '\n';
+
+int main(){
+	memset(dp,-1,sizeof(dp));
+	cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> b[i]; 
+    cout << max(go(0, 1, m - 1), go(0, 0, m)) << '\n'; 
     return 0;
 }
