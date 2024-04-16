48fe8fdd7a9c6ed158b8f57dff06b5b4177e6fc4
pooh <cktjdgus45@naver.com>
2024-04-16 11:09:33 +0900

    5557 : 1학년

--- app.cpp
+++ app.cpp
@@ -1,26 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int cost, n, m1, m2, c;
-int dp[100004];
+typedef long long ll;
+ll n, a[104], dp[104][21];
+ll go(int idx, int sum)
+{
+    if (sum < 0 || sum > 20)
+        return 0;
+    ll &ret = dp[idx][sum];
+    if (ret)
+        return ret;
+    if (idx == n - 2)
+    {
+        if (sum == a[n - 1])
+            return 1;
+        return 0;
+    }
+    ret += go(idx + 1, sum + a[idx + 1]);
+    ret += go(idx + 1, sum - a[idx + 1]);
+    return ret;
+}
 int main()
 {
-    while (1)
+    cin >> n;
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d %d.%d", &n, &m1, &m2);
-        if (n == 0)
-            break;
-        int cost = m1 * 100 + m2;
-        memset(dp, 0, sizeof(dp));
-        for (int i = 0; i < n; i++)
-        {
-            scanf("%d %d.%d", &c, &m1, &m2);
-            int p = m1 * 100 + m2;
-            for (int j = p; j <= cost; j++)
-            {
-                dp[j] = max(dp[j], dp[j - p] + c);
-            }
-        }
-        printf("%d\n", dp[cost]);
+        cin >> a[i];
     }
+    cout << go(0, a[0]) << "\n";
     return 0;
 }
