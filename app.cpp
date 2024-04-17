07cac4bdd672c1410ccb97a8684c68f38bfd6a8a
pooh <cktjdgus45@naver.com>
2024-04-17 11:34:25 +0900

    1238 - 파티

--- app.cpp
+++ app.cpp
@@ -1,31 +1,55 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <queue>
 using namespace std;
-typedef long long ll;
-ll n, a[104], dp[104][21];
-ll go(int idx, int sum)
+int N, M, X, u, v, w, ret;
+vector<pair<int, int>> adj[1001], adj2[1001];
+int dist[1001], dist2[1001];
+bool visited[1001];
+const int INF = 987654321;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+void dijkstra(int *dist, vector<pair<int, int>> *adj)
 {
-    if (sum < 0 || sum > 20)
-        return 0;
-    ll &ret = dp[idx][sum];
-    if (ret)
-        return ret;
-    if (idx == n - 2)
-    {
-        if (sum == a[n - 1])
-            return 1;
-        return 0;
-    }
-    ret += go(idx + 1, sum + a[idx + 1]);
-    ret += go(idx + 1, sum - a[idx + 1]);
-    return ret;
+	pq.push(make_pair(0, X));
+	dist[X] = 0;
+	while (pq.size())
+	{
+		int here = pq.top().second;
+		int here_dist = pq.top().first;
+		pq.pop();
+		if (dist[here] != here_dist)
+			continue;
+		for (pair<int, int> there : adj[here])
+		{
+			int _dist = there.first, _there = there.second;
+			if (dist[_there] > dist[here] + _dist)
+			{
+				dist[_there] = dist[here] + _dist;
+				pq.push(make_pair(dist[_there], _there));
+			}
+		}
+	}
 }
 int main()
 {
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
-    cout << go(0, a[0]) << "\n";
-    return 0;
+	scanf("%d %d %d", &N, &M, &X);
+	fill(dist, dist + 1001, INF);
+	fill(dist2, dist2 + 1001, INF);
+	for (int i = 0; i < M; i++)
+	{
+		scanf("%d %d %d", &u, &v, &w);
+		adj[u].push_back(make_pair(w, v));
+		adj2[v].push_back(make_pair(w, u));
+	}
+	dijkstra(dist, adj);
+	dijkstra(dist2, adj2);
+	for (int i = 1; i <= N; i++)
+	{
+		if (i == X)
+			continue;
+		ret = max(ret, dist[i] + dist2[i]);
+	}
+	printf("%d\n", ret);
+	return 0;
 }
